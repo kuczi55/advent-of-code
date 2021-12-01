25c133cc08b25185ae99baf7cc08a7f92b466091
Kamil Koczera <Koczera9@wp.pl>
2021-12-01 17:49:27 +0100

    Refactor first task

--- 2021/1/src/main/java/adventofcode/sonarsweep/SonarSweep.java
+++ 2021/1/src/main/java/adventofcode/sonarsweep/SonarSweep.java
@@ -1,44 +1,38 @@
 package adventofcode.sonarsweep;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class SonarSweep {
-    public static List<Integer> fileToIntegerList(String filename) {
-        List<Integer> numbers = new ArrayList<>();
+    public static List<Integer> fileToIntegerList(String filename) throws IOException {
 
-        try {
-            File input = new File(filename);
-            Scanner scanner = new Scanner(input);
-
-            while(scanner.hasNext()) {
-                String number = scanner.nextLine();
-                numbers.add(Integer.valueOf(number));
-            }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        return numbers;
+            return Files
+                    .lines(Paths.get(filename))
+                    .map(Integer::valueOf)
+                    .collect(Collectors.toList());
     }
 
     public static void main(String[] args) {
-        List<Integer> numbers = fileToIntegerList("src/main/resources/input.txt");
         int counter = 0;
-        if(numbers.size() > 3) {
-            int prevSum = numbers.get(0) + numbers.get(1) + numbers.get(2);
 
-            for(int i = 1; i < numbers.size() - 2; i++) {
-                int sum = numbers.get(i) + numbers.get(i + 1) + numbers.get(i + 2);
-                if(sum > prevSum) {
-                    counter++;
+        try {
+            List<Integer> numbers = fileToIntegerList("src/main/resources/input.txt");
+            if(numbers.size() > 3) {
+                int prevSum = numbers.get(0) + numbers.get(1) + numbers.get(2);
+
+                for(int i = 1; i < numbers.size() - 2; i++) {
+                    int sum = numbers.get(i) + numbers.get(i + 1) + numbers.get(i + 2);
+                    if(sum > prevSum) {
+                        counter++;
+                    }
+                    prevSum = sum;
                 }
-                prevSum = sum;
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         System.out.println(counter);
